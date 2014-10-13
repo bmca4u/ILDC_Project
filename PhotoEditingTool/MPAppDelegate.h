9e7f1fc9567904030a68300b4010eb502b3ba50d
Bejoy Balan <bmca4u@gmail.com>
2014-10-13 08:25:15 +0530

    new Project Started.

---
+++ PhotoEditingTool/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  PhotoEditingTool
+//
+//  Created by indianic on 13/10/14.
+//  Copyright (c) 2014 ILDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
